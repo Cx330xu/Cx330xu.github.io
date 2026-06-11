cef22cc7dcc3bd092f36a2267570625e139c3108
Cx330xu <3051568508@qq.com>
2026-06-11 15:14:03 +0800

    移除LinkedIn(无真实URL+国内市场无信号价值)和索取简历(与EmailLink功能重叠)

--- src/data/site.ts
+++ src/data/site.ts
@@ -29,7 +29,6 @@ export const profile = {
   email: 'cx330xu@qq.com',
   github: 'https://github.com/Cx330xu',
   gitee: 'https://gitee.com/cx330xu_youyaxiansheng',
-  linkedin: 'https://www.linkedin.com/in/cx330xu',
   csdn: 'https://blog.csdn.net/Xu_youyaxianshen?type=blog',
   avatar: 'https://github.com/Cx330xu.png',
   ogImage: '/og-image.png',
